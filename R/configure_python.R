58dce26fc072494407a37dc7ab7e2f0aae22654d
Theodore Gercken <tgercken1@gmail.com>
2025-08-19 12:50:47 -0700

    Modified configure_python (and related files) to use a virtual env for better integration with spacyr

--- R/configure_python.R
+++ R/configure_python.R
@@ -1,46 +1,36 @@
-# Create environment at load to store ManifestoBERTA in
 iscores_environment <- new.env(parent = emptyenv())
 
 #' Helper function to setup python tools required for processing platforms
 #'
-#' @param env_name Name of the conda environment to setup ("iscores" by default)
-#' @param install Whether to install missing packages (TRUE by default)
-#' @param python_version Version of Python to use if a new environment must be created ("latest" by default)
+#' @param env_name Name of the python virtual environment to setup ("iscores" by default)
 #' @param manifestoberta_model_id Version of the ManifestoBERTA model to use ("2024-1-1" by default)
 #' @return Returns invisible(TRUE) when setup is successful, otherwise it provides a descriptive error
 #' @export
 
-configure_python <- function(env_name = "iscores", install = TRUE, python_version = "latest", manifestoberta_model_id = "2024-1-1") {
-  # Setup Reticulate
-  conda_path <- tryCatch(reticulate::conda_binary("auto"), error = function(e) "") # If conda is not installed (it has no path), install it
-  if (!nzchar(conda_path)) {
-    if (isFALSE(install)) stop("Miniconda is not installed. Please run `configure_python(install = TRUE)` first.")
-    reticulate::install_miniconda()
-  }
-  if (reticulate::py_available(initialize = FALSE)) { # If the conda environment is already bound, check to make sure it is the correct one
-    if (!identical(reticulate::py_discover_config()$python_env_name, env_name)) stop("The conda environment is already bound. Please restart R and run `configure_python()` again.")
+configure_python <- function(env_name = "iscores", manifestoberta_model_id = "2024-1-1") {
+  # Setup Spacy
+  environment <- basename(dirname(dirname(reticulate::py_config()$python)))
+  if (environment != env_name) {
+    stop("There is a different, currently active python environment. Please restart R and then run `configure_python()` again.")
   }
 
-  if (!(env_name %in% reticulate::conda_list()$name)) { # If there is no conda environment with the correct name, create one
-    if (isFALSE(install)) stop(paste("Conda environment", env_name, "does not exist. Please run `configure_python(install = TRUE)` first."))
-    python_version <- if (identical(python_version, "latest")) "python" else paste0("python=", python_version)
-    reticulate::conda_create(env_name, packages = python_version, channel = "conda-forge")
+  Sys.setenv(SPACY_PYTHON = env_name)
+  if (!(env_name %in% reticulate::virtualenv_list())) {
+    reticulate::virtualenv_create(envname = env_name, python = Sys.which("python3"))
   }
-  reticulate::use_condaenv(env_name, required = TRUE) # Bind the conda environment
-
-  missing_packages <- setdiff(c("transformers", "torch", "sentencepiece", "spacy"), tolower(reticulate::py_list_packages(env_name)$package)) # Install the missing required packages into the environment
-  if (length(missing_packages)) {
-    if (isFALSE(install)) stop(paste("The python packages: ", paste(missing_packages, collapse = ", "), " are not installed. Please run `configure_python(install = TRUE)` first."))
-    reticulate::conda_install(env_name, missing_packages, pip = TRUE)
-  }
+  spacyr::spacy_install()
+  try(spacyr::spacy_finalize(), silent = TRUE)
+  spacyr::spacy_initialize()
 
   # Setup ManifestoBERTA
+  reticulate::virtualenv_install(envname = Sys.getenv("SPACY_PYTHON"), packages = c("transformers", "torch", "sentencepiece"), ignore_installed = TRUE)
   huggingfaceR::hf_python_depends()
   model_id <- paste0("manifesto-project/manifestoberta-xlm-roberta-56policy-topics-context-", manifestoberta_model_id)
-  if (identical(iscores_environment$modelId, model_id)) { # Skip if the ManifestoBERTA model is already loaded
+  if (identical(iscores_environment$modelId, model_id)) {
     return(invisible(TRUE))
   }
-  iscores_environment$model <- huggingfaceR::hf_load_pipeline(model_id, task = "text-classification", tokenizer = "xlm-roberta-large", truncation = TRUE, max_length = 512L, trust_remote_code = TRUE, top_k = NULL) # Load the ManifestoBERTA model
+
+  iscores_environment$model <- huggingfaceR::hf_load_pipeline(model_id, task = "text-classification", tokenizer = "xlm-roberta-large", truncation = TRUE, max_length = 512L, trust_remote_code = TRUE, top_k = NULL)
   iscores_environment$modelId <- model_id
 
   invisible(TRUE)
--- tests/testthat/test-configure_python.R
+++ tests/testthat/test-configure_python.R
@@ -2,28 +2,39 @@ setup_test <- function() {
   # Create test environment
   test <- new.env(parent = emptyenv())
 
+  # Defer cleanup
+  withr::defer(
+    {
+      environment <- IScorePackage:::iscores_environment
+      rm(list = ls(envir = environment, all.names = TRUE), envir = environment)
+      rm(list = ls(envir = test, all.names = TRUE), envir = test)
+    },
+    envir = testthat::teardown_env()
+  )
+
   # Create test flags
-  test$miniconda_installed <- FALSE
   test$created_env <- NULL
   test$installed_packages <- NULL
 
-  # Mock reticulate and huggingfaceR functions that depend on an active python environment
+  # Mock functions that depend on an active python environment
   local_mocked_bindings(
-    conda_binary = function(...) "",
-    install_miniconda = function(...) test$miniconda_installed <- TRUE,
-    py_available = function(...) FALSE,
-    py_discover_config = function(...) list(python_env_name = ""),
-    conda_list = function(...) data.frame(name = character()),
-    conda_create = function(env_name, ...) test$created_env <- env_name,
-    use_condaenv = function(...) TRUE,
-    py_list_packages = function(...) data.frame(package = character()),
-    conda_install = function(env_name, packages, ...) test$installed_packages <- packages,
+    spacy_install = function(...) TRUE,
+    spacy_finalize = function(...) TRUE,
+    spacy_initialize = function(...) TRUE,
+    .package = "spacyr",
+    .env = parent.frame()
+  )
+  local_mocked_bindings(
+    py_config = function(...) list(python = "/Users/User/.virtualenvs/iscores/bin/python"),
+    virtualenv_list = function(...) c(""),
+    virtualenv_create = function(envname, ...) test$created_env <- envname,
+    virtualenv_install = function(env_name, packages, ...) test$installed_packages <- packages,
     .package = "reticulate",
     .env = parent.frame()
   )
   local_mocked_bindings(
-    `hf_python_depends` = function(...) TRUE,
-    `hf_load_pipeline` = function(...) "mock_pipeline",
+    hf_python_depends = function(...) TRUE,
+    hf_load_pipeline = function(...) "mock_pipeline",
     .package = "huggingfaceR",
     .env = parent.frame()
   )
@@ -31,30 +42,18 @@ setup_test <- function() {
   test
 }
 
-test_that("configure_python creates environments correctly", {
-  # Ensure package environment is cleaned after test
-  withr::defer({
-    rm(list = ls(envir = iscores_environment), envir = iscores_environment)
-  })
+test_that("configure_python correctly steps through setup process", {
   test <- setup_test()
 
-  # Test that the function runs without error and fully sets up the environment
   expect_invisible(configure_python())
-  expect_true(test$miniconda_installed)
   expect_equal(test$created_env, "iscores")
-  expect_setequal(test$installed_packages, c("transformers", "torch", "sentencepiece", "spacy"))
+  expect_setequal(test$installed_packages, c("transformers", "torch", "sentencepiece"))
   expect_identical(iscores_environment$model, "mock_pipeline")
+  expect_identical(iscores_environment$modelId, "manifesto-project/manifestoberta-xlm-roberta-56policy-topics-context-2024-1-1")
 })
 
-test_that("configure_python handles needed but unauthorized installations correctly", {
-  withr::defer({
-    rm(list = ls(envir = iscores_environment), envir = iscores_environment)
-  })
+test_that("configure_python handles already-bound environment correctly", {
   test <- setup_test()
 
-  # Test that the function throws the correct error when the user provides an install = FALSE flag and check that it does not install anything
-  expect_error(configure_python(install = FALSE), "Miniconda is not installed. Please run `configure_python\\(install = TRUE\\)` first.")
-  expect_false(test$miniconda_installed)
-  expect_null(test$created_env)
-  expect_null(test$installed_packages)
+  expect_error(configure_python(env_name = "iscores_two"), regexp = "different, currently active python environment")
 })
