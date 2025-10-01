ca37332f28c37a84beb8944c66e0f30719e74165
Theodore Gercken <tgercken1@gmail.com>
2025-10-01 06:58:43 -0400

    Fixed python configuration

--- R/configure_python.R
+++ R/configure_python.R
@@ -6,23 +6,25 @@
 #' @export
 
 configure_python <- function(env_name = "iscores", manifestoberta_model_id = "2024-1-1") {
-  # Setup Spacy
-  if (!(env_name %in% reticulate::virtualenv_list())) {
-    reticulate::virtualenv_create(envname = env_name, python = Sys.which("python3"))
+  # Ensure Python Is Not Bound To Another Environment
+  Sys.setenv(RETICULATE_AUTOCONFIGURE = "FALSE")
+  if (reticulate::py_available(initialize = FALSE)) {
+    current_env <- basename(reticulate::py_config()$virtualenv)
+    if (current_env != env_name) rlang::abort(paste0("Python is already bound to the '", current_env, "' environment. Please restart R and try again."))
   }
+  if (!env_name %in% reticulate::virtualenv_list()) reticulate::virtualenv_create(envname = env_name)
   reticulate::use_virtualenv(env_name, required = TRUE)
 
-  python_binary <- reticulate::virtualenv_python(env_name)
-  suppressWarnings(spacyr::spacy_install(conda = FALSE, envname = env_name, python_executable = python_binary))
-  try(spacyr::spacy_finalize(), silent = TRUE)
-  suppressMessages(spacyr::spacy_initialize(python_executable = python_binary))
+  # Setup Python Tools
+  reticulate::virtualenv_install(envname = env_name, packages = c("spacy", "transformers", "torch", "sentencepiece"))
 
-  # Setup ManifestoBERTA
-  reticulate::virtualenv_install(envname = env_name, packages = c("transformers", "torch", "sentencepiece"), ignore_installed = TRUE)
+  path <- reticulate::py_config()$virtualenv
+  Sys.setenv(SPACY_PYTHON = path)
+  options(spacyr.python_executable = path)
+  spacyr::spacy_download_langmodel("en_core_web_sm")
+
   huggingfaceR::hf_python_depends()
-
-  model_id <- paste0("manifesto-project/manifestoberta-xlm-roberta-56policy-topics-context-", manifestoberta_model_id)
-  iscores_environment$model <- huggingfaceR::hf_load_pipeline(model_id, task = "text-classification", tokenizer = "xlm-roberta-large", truncation = TRUE, max_length = 512L, trust_remote_code = TRUE, top_k = NULL)
+  iscores_environment[["model"]] <- huggingfaceR::hf_load_pipeline(paste0("manifesto-project/manifestoberta-xlm-roberta-56policy-topics-context-", manifestoberta_model_id), task = "text-classification", tokenizer = "xlm-roberta-large", truncation = TRUE, max_length = 512L, trust_remote_code = TRUE, top_k = NULL)
 
   invisible(TRUE)
 }
